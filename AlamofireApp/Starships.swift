a1df87948df11880b377a9ba5ac678a2793363bd
maximsamus <maximsamus1@gmail.com>
2022-06-15 15:15:25 +0300

    Models init

---
+++ AlamofireApp/Starships.swift
@@ -0,0 +1,18 @@
+//
+//  Starships.swift
+//  AlamofireApp
+//
+//  Created by Максим Самусь on 15.06.2022.
+//
+
+import Foundation
+
+struct Starships: Decodable {
+  var count: Int
+  var all: [Starship]
+  
+  enum CodingKeys: String, CodingKey {
+    case count
+    case all = "results"
+  }
+}
