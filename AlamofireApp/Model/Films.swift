0ee82fadd4316c24f583fdc145f3f0521f72a70c
maximsamus <maximsamus1@gmail.com>
2022-06-15 15:19:34 +0300

    models fix

---
+++ AlamofireApp/Model/Films.swift
@@ -0,0 +1,18 @@
+//
+//  Films.swift
+//  AlamofireApp
+//
+//  Created by Максим Самусь on 15.06.2022.
+//
+
+import Foundation
+
+struct Films: Decodable {
+  let count: Int
+  let all: [Film]
+  
+  enum CodingKeys: String, CodingKey {
+    case count
+    case all = "results"
+  }
+}
